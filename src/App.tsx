01204915118edf44da4aeb4a62f7fc05f806a731
Kévin DANG <kdang@pitchy.fr>
2022-09-26 12:01:57 +0200

    Pomodoro app

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,108 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { Button, Flex, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import { sendNotification } from '@tauri-apps/api/notification'
+import { ask } from '@tauri-apps/api/dialog'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const [time, setTime] = useState(0)
+  const [timerStart, setTimerStart] = useState(false)
+  const buttons = [
+    {
+      value: 900,
+      display: '15 minutes',
+    },
+    {
+      value: 1800,
+      display: '30 minutes',
+    },
+    {
+      value: 3600,
+      display: '60 minutes',
+    },
+  ]
+  const toggleTimer = () => {
+    setTimerStart(!timerStart)
   }
 
+  const triggerResetDialog = async () => {
+    let shouldReset = await ask('Do you want to reset timer ?', {
+      title: 'Pomodoro Timer App',
+      type: 'warning',
+    })
+    if (shouldReset) {
+      setTime(900)
+      setTimerStart(false)
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerStart) {
+        if (time > 0) {
+          setTime(time - 1)
+        } else if (time === 0) {
+          sendNotification({
+            title: `Time's up!`,
+            body: `Congrats on completing a session ! 🎉`,
+          })
+          clearInterval(interval)
+        }
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [timerStart, time])
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-      <p>{greetMsg}</p>
+    <div className="App" style={{ height: '100%' }}>
+      <Flex
+        background="gray.700"
+        height="100%"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Text color="white" fontWeight="bold" marginTop="20" fontSize="35">
+          Pomodoro Timer
+        </Text>
+        <Text fontWeight="bold" fontSize="7xl" color="white">
+          {`${
+            Math.floor(time / 60) < 10
+              ? `0${Math.floor(time / 60)}`
+              : `${Math.floor(time / 60)}`
+          }:${time % 60 < 10 ? `0${time % 60}` : time % 60}`}
+        </Text>
+        <Flex>
+          <Button
+            width="7rem"
+            background="tomato"
+            color="white"
+            onClick={toggleTimer}
+          >
+            {!timerStart ? 'Start' : 'Pause'}
+          </Button>
+          <Button
+            background={'blue.300'}
+            marginX={5}
+            onClick={triggerResetDialog}
+          >
+            reset
+          </Button>
+        </Flex>
+        <Flex marginTop={10}>
+          {buttons.map(({ value, display }) => (
+            <Button
+              marginX={4}
+              background="green.300"
+              color="white"
+              onClick={() => {
+                setTimerStart(false)
+                setTime(value)
+              }}
+            >
+              {display}
+            </Button>
+          ))}
+        </Flex>
+      </Flex>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
